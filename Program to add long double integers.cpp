42b957ec8d772716287d1efbdb1e30b44945baaa
vaishnavi-pote-2106 <vaishnavipote713@gmail.com>
2024-10-05 09:21:07 +0530

    Add files via upload

---
+++ Program to add long double integers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+float a= 2.10f;
+double b= 2.1034688906;
+long double c= 2.10346889062908;
+std::cout<<"float value:"<<a<<std::endl;
+std::cout<<"double value:"<<b<<std::endl;
+std::cout<<"long double value:"<<c<<std::endl;
+return 0;
+}
