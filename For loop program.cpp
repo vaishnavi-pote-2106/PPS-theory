c3ea50ea35d91ce30af19898e94faeca02c31d54
vaishnavi-pote-2106 <vaishnavipote713@gmail.com>
2024-10-05 09:20:12 +0530

    Add files via upload
    This is the C++ program of for loop

---
+++ For loop program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+// Example of a for loop
+for (int i = 6; i <= 9; ++i) {
+cout << i << " ";
+}
+cout << endl; // To end the line after printing the numbers
+
+return 0;
+}
